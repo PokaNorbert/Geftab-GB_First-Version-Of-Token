44588520f676e2bbfbe6cf12ba427b237e53fd6c
Poka Norbert <86323632+PokaNorbert@users.noreply.github.com>
2025-01-06 02:10:08 +0200

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -1,4 +1,5 @@
-/// This is the first version of the official Geftab cryptocurrency (currently without its own blockchain, but in the form of a BSC token; there is also an unfinished blockchain that can be presented upon request).
+/// This is the first version of the official Geftab cryptocurrency 
+(currently without its own blockchain, but in the form of a BSC token; there is also an unfinished blockchain that can be presented upon request).
 
 pragma solidity 0.5.16;
 
